1f5909207d5585a255555601688755bf94e4b154
harry61033 <harry61033@gmail.com>
2017-03-21 22:19:22 +0800

    聯絡方式
    聯絡方式

---
+++ js/b2.js
@@ -0,0 +1,16 @@
+$(function(){
+	$("nav>ul>li").on({
+		mouseenter:function(){
+			$(this).css("background-color","#708090");
+			$(this).children("div").stop().slideDown();
+		},
+		mouseleave:function(){
+			$(this).css("background-color","#0000");
+			$(this).children("div").stop().slideUp();
+		},
+		click:function(){
+			$(this).css("background-color","#708090");
+			$(this).children("div").stop().slideDown();
+		}
+	});
+});
